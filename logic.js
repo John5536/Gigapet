8a333b14c127d12c337d960fab1ba0b90f6f3b58
John5536 <rrivers52@gmail.com>
2024-11-30 09:14:10 -0500

    testbackground loaded

--- logic.js
+++ logic.js
@@ -2,30 +2,30 @@
 let egg_level = 0;
 let apple = 1;
 let bread = 1;
-let vegetables = .5;
+let vegetables = 0.5;
 let steak = 5;
+
 class egg1 {
-    constructor(type,level,vitality,strength,hunger,happiness){
-        this.type =type;
+    constructor(type, level, vitality, strength, hunger, happiness) {
+        this.type = type;
         this.level = level;
         this.vitality = vitality;
         this.strength = strength;
         this.hunger = hunger;
-        this.happiness = happiness
+        this.happiness = happiness;
     }
 }
 
-function background1 (){
-    let testbackground = document.createElement("img");
-    Image.src ="./img/backgroundtest.jpg" ;
+let canvas = document.getElementById('first_canvas');
+let ctx = canvas.getContext('2d');
+let img = new Image();
+let desiredWidth = 550; // Adjust this value 
+let desiredHeight = 400; // Adjust this value
+img.src = "./img/backgroundtest.jpg"; 
 
-    let container = document.getElementById("canvas_container");
 
-    container.appendChild(testbackground);
-}
-
-document.addEventListener('DOMContentLoaded', function() { 
-    background1(); });
-
+img.onload = function() {
+    ctx.drawImage(img, 0, 0, desiredWidth, desiredHeight); 
+};
 
 
