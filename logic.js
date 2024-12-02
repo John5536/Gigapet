178ac1ad63ae1b31bc0a258672dd29e39e904361
john <rrivers52@gmail.com>
2024-12-02 12:44:19 -0500

    classes have been made, new page added, specifics of each eggs added.

--- logic.js
+++ logic.js
@@ -5,39 +5,56 @@ let bread = 1;
 let vegetables = 0.5;
 let steak = 5;
 
-class egg1 {
-    constructor(type, level, vitality, strength, hunger, happiness) {
+class eggs {
+    constructor(type, level, vitality, strength, hunger, happiness, sprite) {
         this.type = type;
         this.level = level;
         this.vitality = vitality;
         this.strength = strength;
         this.hunger = hunger;
         this.happiness = happiness;
+        this.sprite = sprite;
     }
 }
 
-let canvas = document.getElementById('first_canvas');
+//Home background
+let Width =500;
+let Height = 200;
+let canvas = document.getElementById("first_canvas");
 let ctx = canvas.getContext('2d');
-let video = document.createElement('video'); 
+let backgroundtest = new Image();
 
-let desiredWidth = 700; 
-let desiredHeight = 700; 
+backgroundtest.src = "./img/backgroundtest.jpg";
 
-video.src = "./img/homebackground.mp4";
-video.autoplay = true; 
-video.muted = true;    
-video.loop = true;    
+backgroundtest.onload = function(){
+    ctx.drawImage(backgroundtest, -30, -50, Width, Height);
+}
 
-video.onloadeddata = function() {
-    video.play(); 
+// Red egg choice
+let fireEggimage = new Image(); 
+fireEggimage.src = "test.img";
+let redEgg = new eggs ("Fire", 0, 5, 0, 0, 10, fireEggimage)
+console.log(redEgg);
 
-    function drawFrame() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height); 
-        ctx.drawImage(video, 0, 0, desiredWidth, desiredHeight); 
-        requestAnimationFrame(drawFrame);
-    }
+// Blue egg choice
+let waterEggimage = new Image(); 
+waterEggimage.src = "test.img";
+let blueEgg = new eggs ("Water", 0, 5, 0, 0, 10, waterEggimage)
+console.log(blueEgg);
 
-    drawFrame(); 
-};
+// Green egg choice
+let grassEggimage = new Image(); 
+grassEggimage.src = "test.img";
+let greenEgg = new eggs ("Grass", 0, 5, 0, 0, 10, grassEggimage)
+console.log(greenEgg);
+
+// Yellow egg choice
+let electricEggimage = new Image(); 
+electricEggimage.src = "test.img";
+let yellowEgg = new eggs ("Electric", 0, 5, 0, 0, 10, electricEggimage)
+console.log(yellowEgg);
+
+
+
 
 
